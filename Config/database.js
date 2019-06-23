dbffd57aa785c4083b0cee8c467d9f3c932b19d3
Theodory <theodoryf@gmail.com>
2019-06-23 16:54:03 +0300

    Add sequelize cli

--- Config/database.js
+++ Config/database.js
@@ -1,16 +1,15 @@
 const Sequelize = require('sequelize');
 
-// Option 1: Passing parameters separately
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: process.env.DB_DIALECT
 });
 
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
+// sequelize
+//   .authenticate()
+//   .then(() => {
+//     console.log('Connection has been established successfully.');
+//   })
+//   .catch(err => {
+//     console.error('Unable to connect to the database:', err);
+//   });
