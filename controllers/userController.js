c5d3a9b7a613b817e6f476449e967be790f5361c
Theodory <theodoryf@gmail.com>
2019-07-03 13:14:53 +0300

    Install express session

--- controllers/userController.js
+++ controllers/userController.js
@@ -9,7 +9,8 @@ exports.register = (req, res) => {
 
 	const errors = validationResult(req);
 	if (!errors.isEmpty()) {
-		return res.status(422).json({ errors: errors.array() });
+		// return res.status(422).json({ errors: errors.array() });
+		return res.send({ messages:  req.flash('info') });
 	}
 	res.send(req.body);
 }
