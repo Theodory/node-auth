96b8f905ff4f32950a4537909b702f6012efa13a
Theodory <theodoryf@gmail.com>
2019-07-03 08:44:21 +0300

    Add user controller

--- routes/register.js
+++ routes/register.js
@@ -1,7 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+var userController = require('../controllers/userController');
+
 router.get('/', (req, res, next) =>{
+  res.render('register');
+});
+
+router.post('/', (req, res, next) =>{
   res.render('register');
 });
 
