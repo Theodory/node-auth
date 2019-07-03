a39041d883047eb758f4803eca3fb41a235542c4
Theodory <theodoryf@gmail.com>
2019-07-03 19:16:36 +0300

    Fix users validations

--- Validations/users.js
+++ Validations/users.js
@@ -6,5 +6,5 @@ exports.user = [
   check('lname').isString().isLength({ min: 3 }).withMessage('Last Name must be at least 3 characters long'),
   check('email').isEmail().withMessage('Email is invalid'),
   check('password').isLength({ min: 6 }).withMessage('Password must be at least 6 chars long')
-    .matches(/\d/).withMessage('must contain a number'),
+    .matches(/\d/).withMessage('Password must contain a number'),
 ];
