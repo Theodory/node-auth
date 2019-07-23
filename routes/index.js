67d69d8e0305ab5c2fc87cc5c987760960fcda78
Theodory <theodoryf@gmail.com>
2019-07-23 10:10:15 +0300

    Add passport

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.render('index', { title: 'Express' });
 });
 
 router.get('/home', function(req, res, next) {
-  res.render('home_another');
+
+	req.flash('errors', JSON.stringify());
+	res.render('home_another');
   //res.send('home_another');
 });
 
