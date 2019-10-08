5de17715b012377a5e587095949a259c61d69168
Theodory <theodoryf@gmail.com>
2019-10-08 16:03:37 +0300

    Code refactoring

--- app.js
+++ app.js
@@ -20,14 +20,21 @@ app.set('view engine', 'html');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(session({ cookie: { maxAge: 60000 }, 
-  secret: 'keybord cat',
-  resave: false, 
-  saveUninitialized: false}));
+app.use(session({
+    cookie: {
+        maxAge: 60000
+    },
+    secret: 'keybord cat',
+    resave: false,
+    saveUninitialized: false
+}));
+
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(flash());
@@ -35,25 +42,26 @@ app.use(flash());
 var routes = require('./routes/webRoutes')
 routes(app);
 require('./Config/passport.js')(passport, models.User);
- 
+
 
 
 
 //catch 404 and forward to error handler
 app.use((req, res, next) => {
-  next(createError(404));
+    //return 404 if http resource not found
+    next(createError(404));
 });
 
 // error handler
-app.use((err, req, res,next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use((err, req, res, next) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 
